2d2b6003d17048e9b3019ae852d43528533859cc
Thierry <thierry.tran54@gmail.com>
2020-10-23 23:51:41 +0200

    Ramasser, poser et lancer + autres
    Asset FP All-in-One + utilisation CameraShake + test cinématique avec blocage des mouvements du joueur + possibilité de ramasser, poser et lancer un objet

---
+++ Projet_UPR_jeu_M2/Assets/FirstPersion AIO Pack/FirstPersonAIO/LaunchCameraShake.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Playables;
+using UnityEngine;
+
+public class LaunchCameraShake : MonoBehaviour
+{
+	public PlayableDirector playableDirector;
+	FirstPersonAIO fp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fp = FindObjectOfType<FirstPersonAIO>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	
+    }
+
+    public void OnCollisionEnter(Collision collision){
+    	if(collision.transform.CompareTag("Player")){
+			/*Debug.Log("Test collision cameraShake");    	
+    		FirstPersonAIO fp = FindObjectOfType<FirstPersonAIO>();
+    		fp.ActivateCameraShake(1, 0.02f);*/
+    		fp.DesactivateMovement();
+    		playableDirector.Play();
+    	}
+    }
+}
---
+++ Projet_UPR_jeu_M2/Assets/FirstPersion AIO Pack/FirstPersonAIO/PickableObject.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObject : MonoBehaviour
+{
+	public Transform player;
+	public Transform playerCam;
+
+	// La force avec laquelle on lance l'objet. Personnalisable par objet portant ce script pour avoir différentes forces de lancer
+	public float throwForce = 10;
+	
+	// S'il y a un joueur assez proche pour porter l'objet ayant ce script
+	private bool hasPlayer = false;
+
+	// Si l'objet est porté
+	private bool beingCarried = false;
+
+	// Détection entre objet porté et un mur ou autre collider
+	private bool touched = false;
+
+	void Update()
+	{
+		// vérifie la distance entre l'objet et le joueur
+		float dist = Vector3.Distance(gameObject.transform.position, player.position);
+
+		// si - ou = 1.9 unités de distance = on peut ramasser
+		if(dist <= 1.9f)
+		{
+			hasPlayer = true;
+		}
+		else
+		{
+			hasPlayer = false;
+		}
+
+		// si on peut ramasser et qu'on appuie sur E = on porte l'objet
+        if (hasPlayer && Input.GetKey(KeyCode.E))
+        {
+            GetComponent< Rigidbody>().isKinematic = true;
+            transform.parent = playerCam;
+            beingCarried = true;
+        }
+
+        // Si on porte l'objet
+        if (beingCarried)
+        {
+            // si l'objet touche un mur / objet avec collider
+            if (touched)
+            {
+                GetComponent< Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                touched = false;
+            }
+
+            // Clique gauche = on jette l'objet
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent< Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                GetComponent< Rigidbody>().AddForce(playerCam.forward * throwForce);
+            }
+            // clique droit on pose l'objet
+            else if (Input.GetMouseButtonDown(1))
+            {
+                GetComponent< Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+            }
+        }
+    }
+
+    // Détection de contact grace au collider is trigger (car OnCollisionEnter ne fonctionne pas avec un isKinematic)
+    void OnTriggerEnter()
+    {
+        if (beingCarried)
+        {
+            touched = true;
+        }
+    }
+}
